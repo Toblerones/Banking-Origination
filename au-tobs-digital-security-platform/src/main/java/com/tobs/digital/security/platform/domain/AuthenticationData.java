f1682d9dfc4a4257305d60f8a99ec45e659c75d2
toblerones <toblerones@hotmail.com>
2020-11-28 15:29:23 +1100

    init projects

---
+++ au-tobs-digital-security-platform/src/main/java/com/tobs/digital/security/platform/domain/AuthenticationData.java
@@ -0,0 +1,107 @@
+package com.tobs.digital.security.platform.domain;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.tobs.digital.security.platform.domain.Customer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * AuthenticationData
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-11-28T15:23:11.212748+11:00[Australia/Sydney]")
+
+public class AuthenticationData   {
+  @JsonProperty("applicationId")
+  private String applicationId;
+
+  @JsonProperty("customer")
+  private Customer customer;
+
+  public AuthenticationData applicationId(String applicationId) {
+    this.applicationId = applicationId;
+    return this;
+  }
+
+  /**
+   * application number
+   * @return applicationId
+  */
+  @ApiModelProperty(value = "application number")
+
+
+  public String getApplicationId() {
+    return applicationId;
+  }
+
+  public void setApplicationId(String applicationId) {
+    this.applicationId = applicationId;
+  }
+
+  public AuthenticationData customer(Customer customer) {
+    this.customer = customer;
+    return this;
+  }
+
+  /**
+   * Get customer
+   * @return customer
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AuthenticationData authenticationData = (AuthenticationData) o;
+    return Objects.equals(this.applicationId, authenticationData.applicationId) &&
+        Objects.equals(this.customer, authenticationData.customer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(applicationId, customer);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AuthenticationData {\n");
+    
+    sb.append("    applicationId: ").append(toIndentedString(applicationId)).append("\n");
+    sb.append("    customer: ").append(toIndentedString(customer)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ au-tobs-origination-digital-form/src/main/java/com/tobs/origination/digital/domain/Product.java
@@ -0,0 +1,150 @@
+package com.tobs.origination.digital.domain;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Product
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-11-28T14:57:28.327430+11:00[Australia/Sydney]")
+
+public class Product   {
+  @JsonProperty("productCodes")
+  @Valid
+  private List<String> productCodes = null;
+
+  @JsonProperty("mainCurrency")
+  private String mainCurrency;
+
+  @JsonProperty("subCurrencies")
+  @Valid
+  private List<String> subCurrencies = null;
+
+  public Product productCodes(List<String> productCodes) {
+    this.productCodes = productCodes;
+    return this;
+  }
+
+  public Product addProductCodesItem(String productCodesItem) {
+    if (this.productCodes == null) {
+      this.productCodes = new ArrayList<>();
+    }
+    this.productCodes.add(productCodesItem);
+    return this;
+  }
+
+  /**
+   * Get productCodes
+   * @return productCodes
+  */
+  @ApiModelProperty(value = "")
+
+
+  public List<String> getProductCodes() {
+    return productCodes;
+  }
+
+  public void setProductCodes(List<String> productCodes) {
+    this.productCodes = productCodes;
+  }
+
+  public Product mainCurrency(String mainCurrency) {
+    this.mainCurrency = mainCurrency;
+    return this;
+  }
+
+  /**
+   * Get mainCurrency
+   * @return mainCurrency
+  */
+  @ApiModelProperty(value = "")
+
+
+  public String getMainCurrency() {
+    return mainCurrency;
+  }
+
+  public void setMainCurrency(String mainCurrency) {
+    this.mainCurrency = mainCurrency;
+  }
+
+  public Product subCurrencies(List<String> subCurrencies) {
+    this.subCurrencies = subCurrencies;
+    return this;
+  }
+
+  public Product addSubCurrenciesItem(String subCurrenciesItem) {
+    if (this.subCurrencies == null) {
+      this.subCurrencies = new ArrayList<>();
+    }
+    this.subCurrencies.add(subCurrenciesItem);
+    return this;
+  }
+
+  /**
+   * Get subCurrencies
+   * @return subCurrencies
+  */
+  @ApiModelProperty(value = "")
+
+
+  public List<String> getSubCurrencies() {
+    return subCurrencies;
+  }
+
+  public void setSubCurrencies(List<String> subCurrencies) {
+    this.subCurrencies = subCurrencies;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Product product = (Product) o;
+    return Objects.equals(this.productCodes, product.productCodes) &&
+        Objects.equals(this.mainCurrency, product.mainCurrency) &&
+        Objects.equals(this.subCurrencies, product.subCurrencies);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(productCodes, mainCurrency, subCurrencies);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Product {\n");
+    
+    sb.append("    productCodes: ").append(toIndentedString(productCodes)).append("\n");
+    sb.append("    mainCurrency: ").append(toIndentedString(mainCurrency)).append("\n");
+    sb.append("    subCurrencies: ").append(toIndentedString(subCurrencies)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
